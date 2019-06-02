b2313ad297b2bf264a6d9dbf1d63f0baedf7bd96
Eric Bragas <ericbragas412@gmail.com>
2019-06-02 10:07:04 -0700

    Implement vessel location requests and writing to ./data/locations.csv

--- stream-vessel-locations/main.py
+++ stream-vessel-locations/main.py
@@ -9,6 +9,7 @@ import requests
 import json
 import yaml
 import logging
+import csv
 from datetime import datetime
 
 logging.basicConfig(level=logging.DEBUG)
@@ -21,11 +22,30 @@ if __name__ == "__main__":
     exec_start = datetime.now()
     logging.debug("Start time: {}".format(exec_start))
 
-    # Read config file
-    with open(os.path.join(os.path.dirname(__file__), "config.yaml")) as fp:
-        config = yaml.load(fp, Loader=yaml.BaseLoader)
+    # Setup
+    curr_dir = os.path.dirname(__file__)
 
-    
+    # Read config file
+    with open(os.path.join(curr_dir, "config.yaml")) as fp:
+        config = yaml.load(fp, Loader=yaml.BaseLoader)
+    logging.debug(config)
+
+    # Request vessel locations
+    params = {"apiaccesscode": config["api_access_code"]}
+    response = requests.get(config["api_url"], params=params)
+    data = response.json()
+    fieldnames = data[0].keys()
+
+    # Write CSV headers if file not exists
+    if not os.path.isfile(os.path.join(curr_dir, "data", "locations.csv")):
+        with open(os.path.join(curr_dir, "data", "locations.csv"), "w", newline="") as fp:
+            writer = csv.DictWriter(fp, fieldnames=fieldnames)
+            writer.writeheader()
+
+    # Write all rows
+    with open(os.path.join(curr_dir, "data", "locations.csv"), "a", newline="") as fp:
+        writer = csv.DictWriter(fp, fieldnames=fieldnames)
+        writer.writerows(data)
 
     # Log end
     exec_end = datetime.now()
